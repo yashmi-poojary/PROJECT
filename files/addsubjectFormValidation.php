9211cd10390f3e57d7e6f79e66b0570a983395e0
yashmi-poojary <93127517+yashmi-poojary@users.noreply.github.com>
2022-01-21 13:46:55 +0530

    Add files via upload

---
+++ files/addsubjectFormValidation.php
@@ -0,0 +1,27 @@
+<?php
+
+include 'connection.php';
+if (isset($_POST['SN']) && isset($_POST['SC']) && isset($_POST['SS'])) {
+    $name = $_POST['SN'];
+    $code = $_POST['SC'];
+    $sem = $_POST['SS'];
+    $course = $_POST['ST'];
+    //  $message = "nTry again.";
+    // echo "<script type='text/javascript'>alert('$message');</script>"
+} else {
+    $message = "dead.";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    die();
+}
+$q = mysqli_query(mysqli_connect("localhost", "root", "", "ttms"), "INSERT INTO subjects VALUES ('$code','$name','$course','$sem',0,'','','')");
+if ($q) {
+    $message = "Subject added.";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    header("Location:addsubjects.php");
+} else {
+    $message = "Username and/or Password incorrect.\\nTry again.";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    // header("Location:index.php");
+
+}
+?>
